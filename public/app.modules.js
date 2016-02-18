667218712c6f0f08f54506c1e5b4a1b4801cf2f0
Michelle Jolliffe <michelle_jolliffe@alumni.brown.edu>
2016-02-18 00:31:30 -0800

    Working on navbar and routing. Need to create login and register view pages that are associated with functionality in controllers. About halfway there.

--- public/app.modules.js
+++ public/app.modules.js
@@ -2,7 +2,7 @@
 "use strict"
 
 angular
-  .module('Font', ['ui.router', 'ngAnimate'])
+  .module('Font', ['ui.router', 'ngAnimate', 'ui.bootstrap'])
   .config(function($httpProvider){
     $httpProvider.interceptors.push('authInterceptor');
   });
